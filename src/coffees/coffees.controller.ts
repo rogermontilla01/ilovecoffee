e29989dd81f0cc87529e3f6dd2094b6af1734218
roger montilla <rogermontilla01@gmail.com>
2021-08-22 18:51:59 -0300

    Dto and Validation Pipes

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -1,5 +1,7 @@
 import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
 import { CoffeesService } from './coffees.service';
+import { CreateCoffeeDto } from './dto/create-coffee.dto';
+import { UpdateCoffeeDto } from './dto/update-coffee.dto';
 
 @Controller('coffees')
 export class CoffeesController {
@@ -14,18 +16,20 @@ export class CoffeesController {
   }
 
   @Get(":id")
-  findOne(@Param("id") id: string) {
-    return this.coffeeService.findOne(id)
+  findOne(@Param("id") id: number) {
+    console.log(typeof id)
+    return this.coffeeService.findOne("" + id)
   }
 
   @Post()
-  create(@Body() body: any) {
-    return this.coffeeService.create(body)
+  create(@Body() createCoffeeDto: CreateCoffeeDto) {
+    console.log(createCoffeeDto instanceof CreateCoffeeDto)
+    return this.coffeeService.create(createCoffeeDto)
   }
 
   @Patch(":id")
-  update(@Param("id") id: string, @Body() body: any) {
-    return this.coffeeService.update(id, body)
+  update(@Param("id") id: string, @Body() updateCoffeeDto: UpdateCoffeeDto) {
+    return this.coffeeService.update(id, updateCoffeeDto)
   }
 
   @Delete(":id")
