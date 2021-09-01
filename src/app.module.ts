555e58ebec49adc0b2793793f58bb12cb2a9ecd1
roger montilla <rogermontilla01@gmail.com>
2021-08-31 22:19:54 -0300

    Add .env and dynamic providers

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,19 +3,30 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CoffeesModule } from './coffees/coffees.module';
+import { CoffeeRatingModule } from './coffee-rating/coffee-rating.module';
+import { ConfigModule } from "@nestjs/config";
+import * as Joi from '@hapi/joi';
 
 @Module({
-  imports: [CoffeesModule, TypeOrmModule.forRoot({
-    type: "postgres",
-    host: "db",
-    port: 5432,
-    username: "postgres",
-    password: "postgres",
-    database: "postgres",
-    autoLoadEntities: true,
-    synchronize: false,
+  imports: [
+    ConfigModule.forRoot({
+      validationSchema: Joi.object({
+        DATABASE_HOST: Joi.required(),
+        DATABASE_PORT: Joi.number().default(5432),
+      })
+    }),
+    CoffeesModule,
+    TypeOrmModule.forRoot({
+      type: "postgres",
+      host: process.env.DATABASE_HOST,
+      port: +process.env.DATABASE_PORT,
+      username: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASS,
+      database: process.env.DATABASE_NAME,
+      autoLoadEntities: true,
+      synchronize: true,
 
-  })],
+    }), CoffeeRatingModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/migrations/1630254776081-CoffeeRefactor.ts
+++ src/migrations/1630254776081-CoffeeRefactor.ts
@@ -4,13 +4,13 @@ export class CoffeeRefactor1630254776081 implements MigrationInterface {
 
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
-      `ALTER TABLE "coffee" RENAME COLUMN "name" TO "title"`
+      `ALTER TABLE "coffee" RENAME COLUMN "title" TO "name"`
     )
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
-      `ALTER TABLE "coffee" RENAME COLUMN "title" TO "name"`
+      `ALTER TABLE "coffee" RENAME COLUMN "name" TO "title"`
     )
   }
 
